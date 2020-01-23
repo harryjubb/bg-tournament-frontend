529701fe113676b4841e13adf028b9644cb8c742
Harry Jubb <harry.jubb@gmail.com>
2020-01-23 23:14:37 +0000

    Outer app container.

--- src/App.tsx
+++ src/App.tsx
@@ -34,15 +34,19 @@ const App: React.FC = () => {
   const classes = useStyles()
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/event/:eventCode" children={() => <Dashboard />} />
-        <Route path="/event/:eventCode/play/add" children={() => <div>add play here</div>} />
-      </Switch>
-    </Router>
+    <Container
+      maxWidth="xl"
+    >
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/event/:eventCode" children={() => <Dashboard />} />
+          <Route path="/event/:eventCode/play/add" children={() => <div>add play here</div>} />
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
