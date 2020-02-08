40ba8a0b2cc9ef85f9f29c8891ae223dcf3b243e
Harry Jubb <harry.jubb@gmail.com>
2020-02-08 01:01:38 +0000

    MVP of play cards.

--- src/routes/event/Dashboard.tsx
+++ src/routes/event/Dashboard.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link, useParams, useRouteMatch} from 'react-router-dom'
 
 import Loading from '../../Loading'
 
 import gql from 'graphql-tag';
 import {useQuery} from '@apollo/react-hooks';
+
+import parseISO from 'date-fns/parseISO'
+import formatDistance from 'date-fns/formatDistance'
+import minDate from 'date-fns/min'
+
+import useInterval from '@use-it/interval';
 
 // @ts-ignore
 import Websocket from 'react-websocket';
@@ -13,6 +19,7 @@ import {withStyles, createStyles, makeStyles, Theme} from '@material-ui/core/sty
 import Hidden from '@material-ui/core/Hidden';
 import Grid from '@material-ui/core/Grid';
 /* import Divider from '@material-ui/core/Divider'; */
+import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
@@ -21,6 +28,10 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Badge from '@material-ui/core/Badge';
 import Chip from '@material-ui/core/Chip';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 import StarsIcon from '@material-ui/icons/Stars';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
@@ -72,6 +83,9 @@ const useStyles = makeStyles((theme: Theme) =>
     inline: {
       display: 'inline',
     },
+    mt: {
+      marginTop: theme.spacing(1)
+    },
     scoreDisplay: {
       marginTop: theme.spacing(0.5)
     },
@@ -112,6 +126,25 @@ query ($eventCode: String!) {
       eventTotalScore (eventCode: $eventCode)
       eventPreviousTotalScore (eventCode: $eventCode)
     }
+    playCount
+    recentPlays (n: 5) {
+      id
+      dateCreated
+      winners {
+        id
+        name
+      }
+      losers {
+        id
+        name
+      }
+      score
+      game {
+        id
+        name
+        imageUrl
+      }
+    }
   }
 }
 `
@@ -142,6 +175,12 @@ const Trend: React.FC<TrendProps> = ({rank, previousRank}) => {
 const Dashboard: React.FC = () => {
 
   const {url} = useRouteMatch();
+
+  const [compareDate, setCompareDate] = useState(new Date())
+
+  useInterval(() => {
+    setCompareDate(new Date())
+  }, 30000)
 
   const classes = useStyles();
   const {eventCode} = useParams()
@@ -207,134 +246,169 @@ const Dashboard: React.FC = () => {
       <Grid
         container
         direction="row"
-        justify="center"
-        alignItems="center"
         spacing={2}
       >
-        <List className={classes.root}>
-          <FlipMove>
-            {processedSortedPlayers.map((player, index) => <div key={`${player.id}`}><ListItem alignItems="flex-start">
-              <ListItemAvatar style={{marginRight: '25px'}}>
-                <StyledBadge badgeContent={`${player.rank}${(nth(player.rank))}`} color='secondary'>
-                  <Avatar alt={player.name} src={player.avatarUrl ?? null}>
-                    {player.initial}
-                  </Avatar>
-                </StyledBadge>
-              </ListItemAvatar>
-              <ListItemText
-                primary={<React.Fragment>
-                  {player.name}
-                  {/* &mdash; {index + 1}{(nth(index + 1))} */}
-                </React.Fragment>}
-                secondary={
-                  <Grid
-                    container
-                    alignItems="center"
-                    spacing={1}
-                    className={classes.scoreDisplay}
-                  >
-                    <Grid item xs={3} sm={2} md={1} lg={1} xl={1}>
-                      <Grid
-                        container
-                        alignItems="center"
-                        spacing={1}
-                        style={{color: 'black'}}
-                      >
-                        <StarsIcon className={classes.pointsDisplay} fontSize="small" />&nbsp;{player.displayedScore}
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={3} sm={2} md={1} lg={1} xl={1}>
-                      <Grid
-                        container
-                        alignItems="center"
-                        spacing={1}
-                        style={{color: 'black'}}
-                      >
-                        <Trend rank={player.rank} previousRank={player.previousRank} />
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={3} sm={2} md={1} lg={1} xl={1}>
-                      <Grid
-                        container
-                        alignItems="center"
-                        spacing={1}
-                        className={classes.winDisplay}
-                      >
-                        <ThumbUpIcon fontSize="small" />&nbsp;&nbsp;{player.wins}
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={3} sm={2} md={1} lg={1} xl={1}>
-                      <Grid
-                        container
-                        alignItems="center"
-                        spacing={1}
-                        className={classes.lossDisplay}
-                      >
-                        <ThumbDownIcon fontSize="small" />&nbsp;&nbsp;{player.losses}
-                      </Grid>
-                    </Grid>
-                    <Hidden xsDown>
+        <Grid item container direction="row" alignItems="flex-start" xs={12} sm={12} md={12} lg={7} xl={7}>
+          <List className={classes.root}>
+            <FlipMove>
+              {processedSortedPlayers.map((player, index) => <div key={`${player.id}`}><ListItem alignItems="flex-start">
+                <ListItemAvatar style={{marginRight: '25px'}}>
+                  <StyledBadge badgeContent={`${player.rank}${(nth(player.rank))}`} color='secondary'>
+                    <Avatar alt={player.name} src={player.avatarUrl ?? null}>
+                      {player.initial}
+                    </Avatar>
+                  </StyledBadge>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={<React.Fragment>
+                    {player.name}
+                    {/* &mdash; {index + 1}{(nth(index + 1))} */}
+                  </React.Fragment>}
+                  secondary={
+                    <Grid
+                      container
+                      alignItems="center"
+                      spacing={1}
+                      className={classes.scoreDisplay}
+                    >
                       <Grid item xs={3} sm={2} md={1} lg={1} xl={1}>
                         <Grid
                           container
                           alignItems="center"
                           spacing={1}
-                          className={classes.ratioDisplay}
+                          style={{color: 'black'}}
                         >
-                          <ThumbsUpDownIcon fontSize="small" />&nbsp;&nbsp;{player.winLossPercent}
+                          <StarsIcon className={classes.pointsDisplay} fontSize="small" />&nbsp;{player.displayedScore}
                         </Grid>
                       </Grid>
-                    </Hidden>
-                    <Hidden smDown>
-                      <Grid item>
+                      <Grid item xs={3} sm={2} md={1} lg={1} xl={1}>
                         <Grid
                           container
                           alignItems="center"
-                          justify="center"
                           spacing={1}
-                          className={classes.ratioDisplay}
+                          style={{color: 'black'}}
                         >
-                          {/* Achievements */}
-                          {
-                            player.wins && player.wins === maxWins ?
-                              <React.Fragment>
-                                <Chip size="small" icon={<ThumbUpIcon />} label="Most wins" />&nbsp;&nbsp;
-                            </React.Fragment>
-                              : null
-                          }
-                          {
-                            player.losses && player.losses === maxLosses ?
-                              <React.Fragment>
-                                <Chip size="small" icon={<ThumbDownIcon />} label="Most losses" />&nbsp;&nbsp;
-                            </React.Fragment>
-                              : null
-                          }
-                          {
-                            player.winLossRatio && player.winLossRatio === maxWinLossRatio ?
-                              <React.Fragment>
-                                <Chip size="small" icon={<ThumbsUpDownIcon />} label="Best W/L ratio" />&nbsp;&nbsp;
-                            </React.Fragment>
-                              : null
-                          }
-                          {
-                            (player.wins || player.losses) && player.wins + player.losses === maxPlays ?
-                              <React.Fragment>
-                                <Chip size="small" icon={<PlayCircleFilledIcon />} label="Most plays" />&nbsp;&nbsp;
-                            </React.Fragment>
-                              : null
-                          }
+                          <Trend rank={player.rank} previousRank={player.previousRank} />
                         </Grid>
                       </Grid>
-                    </Hidden>
-                  </Grid>
-                }
-              />
-            </ListItem>
-              <Divider variant="inset" component="li" />
-            </div>)
+                      <Grid item xs={3} sm={2} md={1} lg={1} xl={1}>
+                        <Grid
+                          container
+                          alignItems="center"
+                          spacing={1}
+                          className={classes.winDisplay}
+                        >
+                          <ThumbUpIcon fontSize="small" />&nbsp;&nbsp;{player.wins}
+                        </Grid>
+                      </Grid>
+                      <Grid item xs={3} sm={2} md={1} lg={1} xl={1}>
+                        <Grid
+                          container
+                          alignItems="center"
+                          spacing={1}
+                          className={classes.lossDisplay}
+                        >
+                          <ThumbDownIcon fontSize="small" />&nbsp;&nbsp;{player.losses}
+                        </Grid>
+                      </Grid>
+                      <Hidden xsDown>
+                        <Grid item xs={3} sm={2} md={1} lg={1} xl={1}>
+                          <Grid
+                            container
+                            alignItems="center"
+                            spacing={1}
+                            className={classes.ratioDisplay}
+                          >
+                            <ThumbsUpDownIcon fontSize="small" />&nbsp;&nbsp;{player.winLossPercent}
+                          </Grid>
+                        </Grid>
+                      </Hidden>
+                      <Hidden smDown>
+                        <Grid item>
+                          <Grid
+                            container
+                            alignItems="center"
+                            justify="center"
+                            spacing={1}
+                            className={classes.ratioDisplay}
+                          >
+                            {/* Achievements */}
+                            {
+                              player.wins && player.wins === maxWins ?
+                                <React.Fragment>
+                                  <Chip size="small" icon={<ThumbUpIcon />} label="Most wins" />&nbsp;&nbsp;
+                            </React.Fragment>
+                                : null
+                            }
+                            {
+                              player.losses && player.losses === maxLosses ?
+                                <React.Fragment>
+                                  <Chip size="small" icon={<ThumbDownIcon />} label="Most losses" />&nbsp;&nbsp;
+                            </React.Fragment>
+                                : null
+                            }
+                            {
+                              player.winLossRatio && player.winLossRatio === maxWinLossRatio ?
+                                <React.Fragment>
+                                  <Chip size="small" icon={<ThumbsUpDownIcon />} label="Best W/L ratio" />&nbsp;&nbsp;
+                            </React.Fragment>
+                                : null
+                            }
+                            {
+                              (player.wins || player.losses) && player.wins + player.losses === maxPlays ?
+                                <React.Fragment>
+                                  <Chip size="small" icon={<PlayCircleFilledIcon />} label="Most plays" />&nbsp;&nbsp;
+                            </React.Fragment>
+                                : null
+                            }
+                          </Grid>
+                        </Grid>
+                      </Hidden>
+                    </Grid>
+                  }
+                />
+              </ListItem>
+                <Divider variant="inset" component="li" />
+              </div>)
+              }
+            </FlipMove>
+          </List>
+        </Grid>
+        <Grid item container direction="row" xs={12} sm={12} md={12} lg={5} xl={5} className={classes.mt}>
+          <Typography variant="h5" gutterBottom>Most recent of {event.playCount} game{event.playCount === 1 ? '' : 's'} played</Typography>
+          <Grid container direction="row" spacing={1} xs={12}>
+            {
+              event?.recentPlays?.map((play: any) => <Grid key={play.id} item xs={12}>
+                <Card style={{display: 'flex'}}>
+                  <CardMedia
+                    style={{minWidth: '100px', minHeight: '50px'}}
+                    component="div"
+                    image={play.game.imageUrl}
+                    title={play.game.name}
+                  />
+                  <CardContent style={{flexGrow: 1}}>
+                    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                      <Typography variant="h5" style={{flexGrow: 1}}>
+                        {play.game.name}
+                      </Typography>
+                      <Typography variant="h5" style={{display: 'flex', alignItems: 'center'}}>
+                        <StarsIcon className={classes.pointsDisplay} />
+                        &nbsp;
+                        {(Math.round(play.score * 100) / 100).toFixed(2)}
+                      </Typography>
+                    </div>
+                    <Typography color="textSecondary">
+                      {formatDistance(minDate([parseISO(play.dateCreated), compareDate]), compareDate, {addSuffix: true})}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      {play.winners.length + play.losers.length} player game won by {play.winners.map((winner: any) => winner.name).join(', ')} against {play.losers.map((loser: any) => loser.name).join(', ')}.
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>) ?? []
             }
-          </FlipMove>
-        </List>
+          </Grid>
+        </Grid>
       </Grid>
       <Link to={`${url}/play/add`}>
         <Fab className={classes.fab} color="primary" aria-label="add">
